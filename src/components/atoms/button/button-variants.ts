b045b91d267d791f65836748976357e31ef1dfa9
andriirwanzahri <andri.zahri@gmail.com>
2025-11-02 14:30:47 +0700

    fix(button): component atom button

--- src/components/atoms/button/button-variants.ts
+++ src/components/atoms/button/button-variants.ts
@@ -6,7 +6,7 @@ export const buttonVariants = cva(
         variants: {
             variant: {
                 default:
-                    'bg- text-primary-foreground hover:bg-primary/90',
+                    'bg-primary text-primary-foreground hover:bg-primary/90',
                 destructive:
                     'bg-destructive text-destructive-foreground hover:bg-destructive/90',
                 outline:
