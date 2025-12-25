785c73bdf976a558278f49dc94e41f93f9654fa5
andriirwanzahri <andri.zahri@gmail.com>
2025-12-25 20:44:58 +0700

    feat: add firebase in Project

---
+++ src/config/firebase.ts
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAj9maIPTgIgftlsVqLAD4UlPmc6asywA8",
+  authDomain: "photography-852a7.firebaseapp.com",
+  projectId: "photography-852a7",
+  storageBucket: "photography-852a7.firebasestorage.app",
+  messagingSenderId: "218559279090",
+  appId: "1:218559279090:web:4ed72112805fcbe522856b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
